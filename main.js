d8e91308fc9aac62d9d3a10f30624f0bd6b7d10e
Giacomo Lavermicocca <glavermi@alice.it>
2021-01-05 17:32:22 +0100

    Upgrade feature
    bluetooth -> with services

--- main.js
+++ main.js
@@ -1,103 +1,212 @@
-NRF.setTxPower(4); // Full Power advertising
-//NRF.setConnectionInterval(200);
+NRF.setTxPower(4);
 
-var zero = Puck.mag();
-var intervalID;
-var intervalResetID;
+const MINUTE = 1000; //60 * 1000;
 
-NRF.setAdvertising({
-  0x180F : [String(Puck.getBatteryPercentage())],
-  0x2a01 : [String(0)]
+const getBatt = () => {
+  const batt = E.getBattery();
+  return `B${batt}`;
+};
+const getTemp = () => {
+  const temp = Math.round(E.getTemperature() * 100) / 100;
+  return `T${temp}`;
+};
+const getPir = () => {
+  const pir = Math.round(Puck.light() * 10000) / 10000;
+  return `P${pir}`;
+};
+const getMag = () => {
+  const coord = Puck.mag();
+  return `M${coord.x}|${coord.y}|${coord.z}`;
+};
+const toCharCodeArray = (utf8) => {
+  if (utf8.length > 20) {
+    utf8 = utf8.substr(0, 19);
+  }
+  var arr = [];
+  for (var i = 0; i < utf8.length; i++) {
+    arr.push(utf8.charCodeAt(i));
+  }
+  return arr;
+};
+
+const blinkRosso = () => {LED1.glow(200, 1000);};
+const blinkVerde = () => {LED2.glow(200, 1000);};
+const blinkBianco = () => {LED3.glow(200, 1000);};
+const blinkBiancoBreve = () => {LED3.glow(200, 600);};
+
+NRF.setServices(
+  {
+    "80b7ee5f-fb59-4714-939e-67703691bd19": {
+      "80b7ee5f-fb60-4714-939e-67703691bd19": {
+        value: [0],
+        maxLen: 19,
+        writable: true,
+        notify: true, // optional, default is false
+        onWrite: function (evt) {
+          // Data comes in as a byte, make it 0..1 range
+          //var n = evt.data[0] / 255;
+          // Send data directly to servo as PWM
+          var data = evt.data;
+          blinkBianco();
+          //send BACK
+          NRF.updateServices({
+            "80b7ee5f-fb59-4714-939e-67703691bd19": {
+              "80b7ee5f-fb60-4714-939e-67703691bd19": {
+                value: toCharCodeArray(data),
+                notify: true,
+              },
+            },
+          });
+        },
+      }
+    },
+  },
+  { uart: false, advertise: ["80b7ee5f-fb59-4714-939e-67703691bd19"] }
+);
+NRF.setAdvertising({}, {name:"Puck.js"});
+
+var idIntervalValues;
+var cnt = 0;
+const startHere = () => {
+  idIntervalValues = setInterval(() => {
+    cnt++;
+    var value;
+    if(cnt % 4 == 0){
+     value = getMag();
+    } else if(cnt % 4 == 1){
+      value = getPir();
+    } else if(cnt % 4 == 2){
+      value = getTemp();
+    } else if(cnt % 4 == 3){
+      value = getBatt();
+    }
+    if(value != null){
+      blinkBiancoBreve();
+      NRF.updateServices({
+        "80b7ee5f-fb59-4714-939e-67703691bd19": {
+          "80b7ee5f-fb60-4714-939e-67703691bd19": {
+            value: toCharCodeArray(value),
+            notify: true,
+            writable: true,
+            readable: true,
+          }
+        },
+      },{ uart: false, advertise: ["80b7ee5f-fb59-4714-939e-67703691bd19"] });
+    }
+  }, MINUTE * 10);
+};
+
+// On disconnect, stop the servo
+NRF.on("disconnect", () => {
+  if (idIntervalValues) clearInterval(idIntervalValues);
+  blinkRosso();
+  address = null;
 });
 
-setWatch(function() {
-  if(intervalID!=null){
-    changeInterval(intervalID, 1000*60*5);
-  }
-  if(idBatt!=null){
-    idBatt = null;
-    clearTimeout(idBatt);
-  }
-  if(idTemp!=null){
-    idTemp = null;
-    clearTimeout(idTemp);
-  }
-  if(idPir!=null){
-    idPir = null;
-    clearTimeout(idPir);
-  }
-  if(idMag!=null){
-    idMag = null;
-    clearTimeout(idMag);
-  }
-  if(intervalResetID != null){
-    intervalResetID = null;
-    clearTimeout(intervalResetID);
-  }
+// On disconnect, stop the servo
+var address;
+NRF.on("connect", (addr) => {
+  startHere();
+  address = addr;
+  blinkVerde();
+});
 
-  NRF.setAdvertising({
-    0x180F : [String(Puck.getBatteryPercentage())],
-    0x2a01 : [String(1)]
-  });
+// -----------------------------------------------------------------
+// ------------------------------ BUTTON ---------------------------
+// -----------------------------------------------------------------
 
-  LED2.set();
-  intervalResetID = setTimeout(()=>{
-    LED2.reset();
-    NRF.setAdvertising({
-      0x180F : [String(Puck.getBatteryPercentage())],
-      0x2a01 : [String(0)]
-    });
-  }, 375*4);
-}, BTN, {edge:"rising", repeat:true, debounce:100});
-
-// ---------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------
-
-function getMag(){
-  p = Puck.mag();
-  p.x -= zero.x;
-  p.y -= zero.y;
-  p.z -= zero.z;
-  var s = Math.sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
-   return Math.round(s * 100 ) / 100;
+var log = print;
+var idIntervalBtn;
+function flagRising(e){
+  if(address){
+    var valueL = 0;
+    if(idIntervalValues){changeInterval(idIntervalValues, MINUTE*10);}
+    idIntervalBtn = setInterval(()=>{
+      var value = ++valueL%10;
+      analogWrite(LED2, value*0.1);
+      NRF.updateServices({
+        "80b7ee5f-fb59-4714-939e-67703691bd19": {
+          "80b7ee5f-fb60-4714-939e-67703691bd19": {
+            value: toCharCodeArray(`S${value}`),
+            notify: true,
+            writable: true,
+            readable: true,
+          }
+        },
+      },{ uart: false, advertise: ["80b7ee5f-fb59-4714-939e-67703691bd19"] });
+    }, 420);
+  }
 }
+var toggle = false;
+function flag(e) {
+  if(address){
+    var l = e.time - e.lastTime;
 
-const setupAdvValues = () => {
-  intervalID = setInterval(()=>{
-    advValues();
-  }, 1000*60*5);
+    digitalWrite(LED1,0);
+    digitalWrite(LED2,0);
+    digitalWrite(LED3,0);
+
+    if(idIntervalBtn != null){
+      clearInterval(idIntervalBtn);
+    }
+
+    if (l > 0.4) {
+      //long
+    } else {
+      //short
+      LED3.glow(500, 1000);
+      toggle = !toggle;
+      var valueS = `S${toggle}`;
+      NRF.updateServices({
+        "80b7ee5f-fb59-4714-939e-67703691bd19": {
+          "80b7ee5f-fb60-4714-939e-67703691bd19": {
+            value: toCharCodeArray(valueS),
+            notify: true,
+            writable: true,
+            readable: true,
+          }
+        },
+      },{ uart: false, advertise: ["80b7ee5f-fb59-4714-939e-67703691bd19"] });
+    }
+  }
+}
+// When button pressed
+setWatch(flag, BTN, { repeat: true, edge: "falling", debounce: 50  });
+setWatch(flagRising, BTN, { repeat: true, edge: "rising", debounce: 50  });
+
+// --------------------------------------------------------------------
+// ----------------------------- GLOW ---------------------------------
+// --------------------------------------------------------------------
+
+// add a new function to all pins to glow
+Pin.prototype.glow = function(milliseconds, Hz) {
+  // see if the user specified a Hz
+  // if she/he did not, we default to 60Hz
+  Hz = ((typeof Hz) === "undefined") ? 60 : Hz;
+  // save the pin: this is important, as in an internal function
+  // 'this' would refer to the function, and no longer the pin.
+  var pin = this;
+  // remember that to achieve a certain Hz, we need to 
+  // have cycles every 1000/Hz milliseconds
+  var cycle = 1000/Hz;
+  // we need to cheat a bit and take pos > 0 since
+  // sin(0) = 0 and digitalPulse does not accept 0
+  var pos = 0.001;
+  // next, we create the function that will call our digital pulse
+  var interval = setInterval(function() {
+    // we send our pulse, and we use the sine function to determine the length
+    // the use of Math.pow( .. ) will epsecially lower the lowest values, while
+    // keeping 1 almost 1. This will help to ensure that only the brightest
+    // part of the glow is truly bright.
+    digitalPulse(pin, 1, Math.pow(Math.sin(pos*Math.PI), 2)*cycle);
+    // we advance to the next position, determined by 
+    // how long we want one glow to take
+    pos += 1/(milliseconds/cycle);
+    // if we are done, we clear this interval
+    if (pos>=1) clearInterval(interval);
+    // finally, we launch this every cycle, where the dimmer cycles
+    // will have a shorter pulse and the brighter cycles a longer pulse
+  }, cycle);
 };
-setupAdvValues();
-advValues();
 
-var idBatt;
-var idTemp;
-var idPir;
-var idMag;
-function advValues() {
-  idBatt = setTimeout(() => {
-    NRF.setAdvertising({
-      0x180F : [String(Puck.getBatteryPercentage())],
-    });
-  }, 375*4);
-  idTemp = setTimeout(() => {
-    var temp = Math.round(E.getTemperature()*1000)/1000;
-    NRF.setAdvertising({
-      0x2a02 : [String(temp)],
-    });
-  }, 375*8);
-  idPir = setTimeout(() => {
-    var pir = Math.round(Puck.light()*1000000)/1000000;
-    NRF.setAdvertising({
-      0x2a03 : [String(pir)]
-    });
-  }, 375*12);
-  idMag = setTimeout(() => {
-    NRF.setAdvertising({
-      0x2a04 : [String(getMag())]
-    });
-  }, 375*16);
-}
-
-save();
+blinkBiancoBreve();
