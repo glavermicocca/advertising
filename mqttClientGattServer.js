5179b1f1706344399abb6fad9d50d6469446fa2f
Giacomo Lavermicocca <glavermi@alice.it>
2021-01-06 00:35:30 +0100

    Fix callback and reconnect
    reconnect after fail

--- mqttClientGattServer.js
+++ mqttClientGattServer.js
@@ -6,13 +6,13 @@ const options = {
 };
 
 const mqtt = require("mqtt");
-const client = mqtt.connect("*****", {
+const client = mqtt.connect("****", {
   clientId: "mqttjs01",
 });
 console.log("connected flag  " + client.connected);
 
 client.on("connect", function (evn) {
-  console.log("connected");
+  //console.log(evn);
 });
 
 //handle errors
@@ -34,9 +34,10 @@ noble.on("stateChange", async (state) => {
   }
 });
 
-setInterval(async () => {
-  await noble.startScanningAsync([SERVICE_UUID], false);
-}, 15000);
+// setInterval(async () => {
+//   console.log(`Restart scanning...`)
+//   await noble.startScanningAsync([SERVICE_UUID], false);
+// }, 20000);
 
 noble.onScanStop((state) => {
   console.log(state);
@@ -45,7 +46,8 @@ noble.onScanStop((state) => {
 noble.on("discover", async (peripheral) => {
   const name = peripheral.advertisement.localName;
   console.log(`Connecting to '${name}' - ${peripheral.address} - ${peripheral.id}`);
-  connectAndSetUp(peripheral);
+  if(peripheral.address != 'fa:bd:39:0c:b8:0a')
+    connectAndSetUp(peripheral);
 });
 noble.on("disconnect", (data) => {
   console.log("disconnected");
@@ -53,81 +55,107 @@ noble.on("disconnect", (data) => {
 });
 
 const message = {};
-
 async function connectAndSetUp(peripheral) {
-  if (peripheral.state != "connected") {
+  if (peripheral.state != "connected" || peripheral.state != "connecting") {
     try {
       
-      const _id = peripheral.id
-      const id = _id.replace(":", "")
-      const tele_puckjs_SENSOR = `tele/${id}/SENSOR`
-      //const address = peripheral.address
-      //console.log(`Peripheral with ID ${_id} address ${address} found`);
-  
-      if(message[id] == null) {message[id] = {}}
-
-      peripheral.once("disconnect", (dev) => {
-        console.log("Disconnect -> " + dev + " " + peripheral);
-      });
       await noble.stopScanningAsync();
+      
       await peripheral.connectAsync();
 
-      const {characteristics,} = await peripheral.discoverSomeServicesAndCharacteristicsAsync([SERVICE_UUID,]);
+      startChar(peripheral)
 
-      console.log("Discovered services and characteristics");
-
-      const characteristic = characteristics[0];
-
-      characteristic.on("data", (data, isNotification) => {
-        console.log(`Received: "${String(data)}"    isNotification: ${isNotification}`);
-
-        var data = String(data);
-
-        if (data.indexOf("B") > -1) {
-          //Battery
-          message[id].batt = String(data.substr(1));
-        } else if (data[0] === "T") {
-          //Temperature
-          message[id].temp = String(data.substr(1));
-        } else if (data[0] === "P") {
-          //Luminosita
-          message[id].pir = String(data.substr(1));
-        } else if (data[0] === "M") {
-          //Temperature
-          var p = data.substr(1).split("|");
-          var x = Number(p[0]);
-          var y = Number(p[1]);
-          var z = Number(p[2]);
-          var s = Math.sqrt(x * x + y * y + z * z);
-          s = Math.round(s * 100) / 100;
-          message[id].mag = String(s);
-        } else if (data[0] === "S") {
-          //Switch
-          if (data.substr(1) === "true") {
-            message[id].toggle = 'on';
-          } else if (data.substr(1) === "false") {
-            message[id].toggle = 'off';
-          } else {
-            var p = data[1] * 10 + 10;
-            message[id].presses = p;
-          }
-        }
-
-        if(client.connected){
-          client.publish(tele_puckjs_SENSOR, JSON.stringify(message[id]), options);
-        }
-      });
-
-      // subscribe to be notified whenever the peripheral update the characteristic
-      characteristic.subscribe((error) => {
-        if (error) {
-          console.error("Error subscribing to echoCharacteristic");
-        } else {
-          console.log("Subscribed for echoCharacteristic notifications");
-        }
-      });
     } catch (error) {
       console.log(error);
     }
   }
+}
+
+async function startChar(peripheral) {  
+
+  const _id = peripheral.id
+  
+  const id = _id.replace(":", "")
+  
+  const tele_puckjs_SENSOR = `tele/${id}/SENSOR`
+  
+  if (message[id] == null) { message[id] = {} }
+
+  if (message[id].idInterval) clearInterval(message[id].idInterval)
+
+  const { characteristics, } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([SERVICE_UUID,]);
+
+  peripheral.on("disconnect", () => {
+    console.log("Disconnect -> " + peripheral);
+    message[id].idInterval = setInterval(async () => {
+      console.log(`Restart scanning...` + peripheral.state)
+      if (peripheral.state != 'connected') {
+        try {
+          peripheral.removeAllListeners()
+          characteristic.removeAllListeners()
+          await peripheral.connectAsync();
+
+          if(peripheral.state == 'connected'){
+            startChar(peripheral)
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }, 2500);
+  });
+
+  console.log("Discovered services and characteristics");
+
+  const characteristic = characteristics[0];
+
+  characteristic.on("data", (data, isNotification) => {
+    console.log(`Received: "${String(data)}"    isNotification: ${isNotification}`);
+
+    var data = String(data);
+
+    if (data.indexOf("B") > -1) {
+      //Battery
+      message[id].batt = String(data.substr(1));
+    } else if (data[0] === "T") {
+      //Temperature
+      message[id].temp = String(data.substr(1));
+    } else if (data[0] === "P") {
+      //Luminosita
+      message[id].pir = String(data.substr(1));
+    } else if (data[0] === "M") {
+      //Temperature
+      var p = data.substr(1).split("|");
+      var x = Number(p[0]);
+      var y = Number(p[1]);
+      var z = Number(p[2]);
+      var s = Math.sqrt(x * x + y * y + z * z);
+      s = Math.round(s * 100) / 100;
+      message[id].mag = String(s);
+    } else if (data[0] === "S") {
+      //Switch
+      if (data.substr(1) === "true") {
+        message[id].toggle = 'on';
+      } else if (data.substr(1) === "false") {
+        message[id].toggle = 'off';
+      } else {
+        var p = data[1] * 10 + 10;
+        message[id].presses = p;
+      }
+    }
+
+    if (client.connected) {
+      client.publish(tele_puckjs_SENSOR, JSON.stringify(message[id]), options);
+    }
+  });
+
+  // subscribe to be notified whenever the peripheral update the characteristic
+  characteristic.subscribe((error) => {
+    if (error) {
+      console.error("Error subscribing to echoCharacteristic");
+      characteristic.removeAllListeners()
+    } else {
+      console.log("Subscribed for echoCharacteristic notifications");
+    }
+  });
 }
